936e2b6228ff440851c6571fc1ff882851bb467d
zhangchengxin <zhangchengxin@meicai.cn>
2018-06-28 12:19:47 +0800

    init

---
+++ components/autocomplete/index.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="autocomplete-wrap" :class="getCustomClass">
+    <div class="autocomplete-input">
+      <input
+        type="text"
+        :value="value"
+        :placeholder="placeholder"
+        @input="handleInput($event.target.value.trim())"
+        @keydown.up="handleArrowUp"
+        @keydown.down="handleArrowDown"
+        @keydown.enter="handleEnter"
+        @click.stop
+        ref="ipt">
+      <ul
+        v-if="listData.length > 0">
+        <li
+          v-for="(item, index) in listData"
+          @click.stop="handleListClick(item)"
+          :key="index"
+          ref="lies">
+          <slot name="item" :item="item" :index="index">
+            {{ item.value }}
+          </slot>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Autocomplete',
+    props: {
+      // 自定义类名
+      customClass: {
+        type: String
+      },
+      // 获取提示数据的逻辑
+      fetchSuggestions: {
+        type: Function
+      },
+      // input 事件的回调
+      onInput: {
+        type: Function
+      },
+      // 双向数据绑定值
+      value: {
+        type: String
+      },
+      placeholder: {
+        type: String,
+        default: '请输入'
+      }
+    },
+    data () {
+      return {
+        /**
+         * 列表数据
+         * 元素必须是对象且含有 value 字段，如：
+         * [{ name: '北京市', value：'北京' }]
+         * 因为，输入框的值，会使用 value 字段
+         */
+        listData: [],
+        // 当前下标
+        index: -1
+      }
+    },
+    created () {
+      this.init()
+    },
+    destroyed () {
+      document.removeEventListener('click', this.hideList)
+    },
+    computed: {
+      getCustomClass () {
+        return [this.customClass]
+      }
+    },
+    methods: {
+      // 初始化
+      init () {
+        // 文档任意处点击隐藏下拉列表
+        document.addEventListener('click', this.hideList)
+      },
+      // 隐藏列表
+      hideList () {
+        this.listData = []
+      },
+      // input 事件
+      handleInput (value) {
+        // 不为空时才进行查询
+        if (value) {
+          this.fetchSuggestions(value, listData => {
+            this.listData = listData
+          })
+        } else {
+          this.listData = []
+        }
+
+        // 有数据时，重置样式
+        if (this.listData.length > 0) {
+          this.resetStyle()
+        }
+
+        // 重置下标
+        this.index = -1
+        // 触发双向绑定的 input 事件
+        this.$emit('input', value)
+        // 触发自定义的 onInput 回调
+        if (typeof this.onInput === 'function') {
+          this.onInput.call(this.$parent)
+        }
+      },
+      // 列表点击事件
+      handleListClick (item) {
+        this.$refs.ipt.value = item.value
+        this.listData = []
+        this.index = -1
+        // 触发双向绑定的 input 事件
+        this.$emit('input', item.value)
+        // 触发自定义的 select 事件
+        this.$emit('select', item)
+      },
+      // 向下箭头事件
+      handleArrowDown () {
+        let len = this.listData.length
+
+        if (len === 0) { return }
+
+        if (++this.index >= len) {
+          this.index = 0
+        }
+
+        this.toIndex()
+      },
+      // 向上箭头事件
+      handleArrowUp () {
+        let len = this.listData.length
+
+        if (len === 0) return
+
+        if (--this.index === -1) {
+          this.index = len - 1
+        }
+
+        this.toIndex()
+      },
+      // 回车
+      handleEnter () {
+        let len = this.listData.length
+
+        if (len === 0) return
+
+        let item = this.listData[this.index]
+
+        this.handleListClick(item)
+      },
+      // 转到第index个项
+      toIndex () {
+        this.resetStyle()
+
+        this.$refs.lies[this.index].classList.add('on')
+
+        let item = this.listData[this.index]
+        this.$refs.ipt.value = item.value
+        this.$emit('input', item.value)
+      },
+      // 重置样式
+      resetStyle () {
+        this.$refs.lies.forEach(el => {
+          el.classList.remove('on')
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .autocomplete-wrap {
+    background-color: #fff;
+  }
+
+  .autocomplete-wrap .autocomplete-input {
+    position: relative;
+    input {
+      display: block;
+      width: 100%;
+      height: 40px;
+      line-height: 1;
+      padding: 0 10px;
+      border: 1px solid #dcdfe6;
+      border-radius: 4px;
+      &:focus {
+        border-color: #409eff;
+      }
+      &::placeholder {
+        opacity: 0.5;
+      }
+      &::-webkit-input-placeholder {
+        opacity: 0.5;
+      }
+      &::-moz-placeholder {
+        opacity: 0.5;
+      }
+      &::-ms-input-placeholder {
+        opacity: 0.5;
+      }
+    }
+    ul {
+      width: 100%;
+      border: 1px solid #eee;
+      border-top: none;
+      position: absolute;
+      top: 100%;
+      z-index: 99;
+      li {
+        height: 30px;
+        line-height: 30px;
+        padding-left: 11px;
+        background-color: #fff;
+        color: #000;
+        cursor: pointer;
+        &:hover, &.on {
+          background-color: #eee;
+          color: #999;
+        }
+      }
+    }
+  }
+</style>
---
+++ http/index.js
@@ -0,0 +1,67 @@
+/**
+ * 必须对 http 请求风筝一层，
+ * 防止 axios 被换成别的库
+ */
+
+import axios from 'axios'
+import { Notification } from 'element-ui'
+
+const $ = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foo'}
+})
+
+// http request 拦截器
+$.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
+// http response 拦截器
+$.interceptors.response.use(
+  response => {
+    // 假设 code: 0 是获取数据失败，但 http 是成功的
+    if (response.data.code === 0) {
+      Notification.error({
+        title: '错误',
+        message: response.data.error ? response.data.error.msg : String(response.data)
+      })
+    }
+
+    return response
+  },
+  error => {
+    Notification.error({
+      title: '异常',
+      message: '服务器异常,请稍后重试'
+    })
+
+    return Promise.reject(error)
+  }
+)
+
+export default {
+  get (url, params, success) {
+    $.get(url, {
+      params
+    }).then(res => {
+      if (typeof success === 'function') {
+        success(res.data)
+      }
+    })
+  },
+  post (url, params, success) {
+    $.post(url, params)
+      .then(res => {
+        if (typeof success === 'function') {
+          success(res.data)
+        }
+      })
+  }
+}
