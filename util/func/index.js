92d579ed1524c0f749c3a812aabc6551e209b850
zhangchengxin <zhangchengxin@meicai.cn>
2018-09-04 19:10:51 +0800

    add: utils

--- util/func/index.js
+++ util/func/index.js
@@ -1,17 +1,4 @@
 /**
- * 保留小数点后 n 位有效数字
- * 直接使用 Number.prototype.toFixed() 会四舍五入
- */
-export function toFixed (num, keep) {
-  if (typeof num !== 'number') {
-    num = Number(num)
-  }
-
-  return Number.parseFloat(num.toFixed(keep + 1).slice(0, -1))
-}
-
-
-/**
  * 根据 页码、每页总数，
  * 从数组中获取第几页的数据
  * @param  {Number} pageNum  页码
@@ -25,4 +12,16 @@ export function getPage (pageNum, pageSize, arr) {
   return (offset + pageSize >= arr.length)
     ? arr.slice(offset, arr.length)
     : arr.slice(offset, offset + pageSize)
+}
+
+/**
+ * 保留小数点后 n 位有效数字
+ * 直接使用 Number.prototype.toFixed() 会四舍五入
+ */
+export function toFixed (num, keep) {
+  if (typeof num !== 'number') {
+    num = Number(num)
+  }
+
+  return Number.parseFloat(num.toFixed(keep + 1).slice(0, -1))
 }
---
+++ util/shared/index.js
@@ -0,0 +1,88 @@
+export function isUndef (v) {
+  return v === undefined || v === null
+}
+
+export function isDef (v) {
+  return v !== undefined && v !== null
+}
+
+export function isTrue (v) {
+  return v === true
+}
+
+export function isFalse (v) {
+  return v === false
+}
+
+/**
+ * Check if value is primitive
+ */
+export function isPrimitive (value) {
+  return (
+    typeof value === 'string' ||
+    typeof value === 'number' ||
+    typeof value === 'symbol' ||
+    typeof value === 'boolean'
+  )
+}
+
+/**
+ * Quick object check - this is primarily used to tell
+ * Objects from primitive values when we know the value
+ * is a JSON-compliant type.
+ */
+export function isObject (obj) {
+  return obj !== null && typeof obj === 'object'
+}
+
+/**
+ * Get the raw type string of a value e.g. [object Object]
+ */
+const _toString = Object.prototype.toString
+
+export function toRawType (value) {
+  return _toString.call(value).slice(8, -1)
+}
+
+/**
+ * Strict object type check. Only returns true
+ * for plain JavaScript objects.
+ */
+export function isPlainObject (obj: any): boolean {
+  return _toString.call(obj) === '[object Object]'
+}
+
+export function isRegExp (v: any): boolean {
+  return _toString.call(v) === '[object RegExp]'
+}
+
+/**
+ * Check whether the object has the property.
+ */
+const hasOwnProperty = Object.prototype.hasOwnProperty
+export function hasOwn (obj, key) {
+  return hasOwnProperty.call(obj, key)
+}
+
+/**
+ * Camelize a hyphen-delimited string.
+ */
+const camelizeRE = /-(\w)/g
+export function camelize (str) {
+  return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')
+}
+
+/**
+ * Capitalize a string.
+ */
+export function capitalize (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+/**
+ * Hyphenate a camelCase string.
+ */
+const hyphenateRE = /\B([A-Z])/g
+export function hyphenate (str) {
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+}
